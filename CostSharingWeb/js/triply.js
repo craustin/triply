eff3b8632b85676663dab2a7d45c0a6ae0db2388
Craig Austin <craig.austin@gmail.com>
2013-08-30 00:30:35 -0400

    Re-add CostSharingWeb. UI is mostly functional. Next step is to hook UI up to model in backbone.js. Then build server API. Then hook both backbone.js and iOS app up to API.

---
+++ CostSharingWeb/js/triply.js
@@ -0,0 +1,104 @@
+$(function(){
+
+  // Make fields editable
+  $.fn.editable.defaults.mode = 'inline';
+  $('#trip-name').editable({ emptytext: 'Trip Name' });
+  $('#edit-cost-name').editable({ emptytext: 'Name' });
+  $('#edit-cost-cost').editable({ emptytext: 'Cost' });
+  $('#edit-cost-paid-by').editable({
+    source: ['Craig', 'Xiaoyi', 'Jim', 'Wendy'],
+    emptytext: 'Paid By'
+  });
+  $('#edit-cost-paid-for').editable({ emptytext: 'Paid For' }); 
+  $('#new-person-name').editable({ emptytext: 'Name' }); 
+
+  // Create model for people 
+  var Person = Backbone.Model.extend({
+    defaults:{
+      name: '',
+      clear_through: false
+    }
+  });
+
+  var PersonList = Backbone.Collection.extend({
+    model: Person
+  });
+
+  var PersonListView = Backbone.View.extend({
+    tagName: 'ul',
+    className: 'nav nav-list',
+    initialize: function(){
+      _.bindAll(this, "renderPerson");
+    },
+    renderPerson: function(model){
+      var personView = new PersonView({model: model});
+      this.$el.append(personView.render());
+    },
+    render: function(){
+      var peopleHeader = '<li class="nav-header">Participants</li>';
+      var peopleFooter = '<li class="divider"></li>' +
+        '<li><a href="#newPersonModal" role="button" data-toggle="modal"><i class="icon-user"></i> Add Person</a></li>';
+
+      this.$el.empty();
+      this.$el.append(peopleHeader);
+      this.collection.each(this.renderPerson);
+      this.$el.append(peopleFooter);
+      return this.$el;
+    }
+  });
+
+  var PersonView = Backbone.View.extend({
+    tagName: 'li',
+    events: {
+      'click a': 'clicked'
+    },
+    initialize: function(){
+      this.listenTo(this.model, 'change', this.render);
+    },
+    render: function(){
+      var star = this.model.get('clear_through') ? ' <i class="icon-star"></i>' : '';
+      this.$el.html('<a href="#">' + this.model.get('name') + star + '</a>');
+      return this.$el;
+    },
+    clicked: function(e){
+      e.preventDefault();
+
+      // upon click, set all clear_through to false and this one to true
+      people.each(function(person){
+        person.set('clear_through', false);
+      }, this);
+      this.model.set('clear_through', true);
+    }
+  });
+
+  var trip_name = 'Zubehaus Weekend';
+  var people = new PersonList([
+    new Person({ name: 'Craig', clear_through: true }),
+    new Person({ name: 'Xiaoyi' }),
+    new Person({ name: 'Jim' }),
+    new Person({ name: 'Wendy' }),
+    new Person({ name: 'Ankur' }),
+    new Person({ name: 'Alex' })
+  ]);
+  window.people = people;
+
+  $('#trip-name').editable('option', 'value', trip_name);
+
+  var renderPersonList = function(){
+    // shouldn't this function be in a view somewhere?
+    var view = new PersonListView({ collection: people });
+    $("#people-list").html(view.render());
+  };
+  renderPersonList();
+
+  $('#new-person-btn').click(function(){
+    var name = $('#new-person-name').editable('getValue')['new-person-name'];
+    if (!name)
+      return;
+    people.add([{ name: name }]);
+    $('#newPersonModal').modal('hide');
+  });
+
+  // do i really need to create a new PersonListView every time we add?
+  people.listenTo(people, 'add', renderPersonList);
+});

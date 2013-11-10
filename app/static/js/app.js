3bdb3ce66ec608c8c4aa28fc39737736519e670f
Eric Rogstad <esrogs@gmail.com>
2013-11-10 10:58:20 -0800

    rendering the people-list using angular

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -9,10 +9,6 @@ angular.module('triplyApp', [
 	'triplyApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/asdf', {templateUrl: 'static/partials/asdf.html', controller: 'AsdfController'});
 	$routeProvider.when('/share-costs', {templateUrl: 'static/partials/cost_sharing.html', controller: 'CostSharingController'});
-
-	// $routeProvider.when('/triply', {templateUrl: 'partials/triply.html', controller: 'TriplyCtrl'});
-	// $routeProvider.when('/triply/trips/:tripId', {templateUrl: 'partials/triply.html', controller: 'TripDetailCtrl'});
-	$routeProvider.otherwise({redirectTo: '/baleeted'});
+	$routeProvider.otherwise({redirectTo: '/share-costs'});
 }]);
--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -4,14 +4,26 @@
 
 console.info('controllers.js');
 
-var controllers = angular.module('triplyApp.controllers', []);
-
-controllers.controller('AsdfController', ['$scope',
-	function AsdfController($scope) {
-		console.info('asdf');
-	}]);
+angular.module('triplyApp.controllers', []);
+var controllers = angular.module('triplyApp.controllers');
 
 controllers.controller('CostSharingController', ['$scope',
 	function CostSharingController($scope) {
-		console.info('cost sharing controller');
+		console.info('cost sharing controller asdf!!!');
+		$scope.peopleList = 'static/partials/people-list.html';
+		console.info($scope.peopleList);
+	}]);
+
+controllers.controller('PeopleListController', ['$scope',
+	function PeopleListController($scope) {
+		console.info('people list controller');
+
+		$scope.people = [
+			{ name: 'Craig' },
+			{ name: 'Xiaoyi' },
+			{ name: 'Jim' },
+			{ name: 'Wendy' },
+			{ name: 'Ankur' },
+			{ name: 'Alex' }
+		];
 	}]);

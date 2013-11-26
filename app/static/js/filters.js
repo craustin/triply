4f6d4d376ca1820b59d99757db4e66be92cf94a7
Craig Austin <craig.austin@gmail.com>
2013-11-25 23:48:42 -0500

    Add comment

--- app/static/js/filters.js
+++ app/static/js/filters.js
@@ -8,6 +8,8 @@ filter('peopleFilter', function() {
 	// the original list. if it doesn't, Angular thinks something changed and
 	// gets stuck in a $digest loop
 	return function(input, scope, flag_sign) {
+		// i'd rather not use "+"/"-" as a flag, but I don't know the idiomatic
+		// way to do this list filtering (http://i.imgur.com/cpLT6MJ.gif)
 		var filteredElements = [];
 		var clearPerson = scope.getClearPerson();
 		angular.forEach(input, function(person) {
